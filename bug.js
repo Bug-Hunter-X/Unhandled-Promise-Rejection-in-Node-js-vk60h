4dd77cee55f2539c16bff4d6b2423d732ad62953
BugHunter-X <askzenai@gmail.com>
2024-12-24 16:42:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,16 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.setHeader('Content-Type', 'application/json');
+  response.writeHead(200);
+  const data = {
+    message: 'Hello from Node.js',
+  };
+  response.end(JSON.stringify(data));
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
